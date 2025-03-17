57a9b56ed51a53a5d35c7afd215edce9a644bdd2
amani387 <negaamanuel387@gmail.com>
2025-03-17 14:55:11 +0300

    port defination

--- app.py
+++ app.py
@@ -29,7 +29,7 @@ warnings.filterwarnings('ignore')
 logging.basicConfig(level=logging.INFO)
 
 app = Flask(__name__)
-CORS(app,resources={r"/*": {"origins": "*"}})  # Allow cross-origin requests
+CORS(app)  # Allow cross-origin requests
 
 # ---------------------------
 # HELPER FUNCTIONS
@@ -687,5 +687,6 @@ def optimize():
         return jsonify({"error": str(e)}), 500
 
 if __name__ == "__main__":
-    app.run(debug=True, port=5000)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port= port)
 
